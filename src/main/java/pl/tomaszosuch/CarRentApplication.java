5a70744f089f7d2e0366a507ab81114a395688e1
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-12-28 11:02:16 +0100

    first commit

---
+++ src/main/java/pl/tomaszosuch/CarRentApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentApplication.class, args);
+    }
+
+}
